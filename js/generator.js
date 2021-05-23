c78f3a5518994857d8358091260876abdfc38f4f
Diacius <77890144+Diacius@users.noreply.github.com>
2021-05-23 12:08:36 +0100

    first commit

---
+++ js/generator.js
@@ -0,0 +1,11 @@
+var Trivia = [
+    "Who invented the World Wide Web?",
+    "Q: In bingo, what number is represented by “two little ducks”? A:22",
+    "In a search bar what does the www mean?",
+    "Pen"
+];
+var randomTrivia = Math.floor(Math.random() * Trivia.length);
+function getTrivia() {
+    randomTrivia[Trivia];
+}
+
