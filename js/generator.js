5440e22ead167797e724c6d9f8c4593f7aae29d6
Diacius <77890144+Diacius@users.noreply.github.com>
2023-01-03 12:15:05 +0000

    localstorage save and load

--- js/generator.js
+++ js/generator.js
@@ -1,17 +1,14 @@
-var triviaQuestions = [
-    "Q: Who invented the World Wide Web?",
-    "Q: In bingo, what number is represented by “two little ducks”?",
-    "Q: In a search bar what does the www mean?",
-    "Q: In which European city would you find the airport “Orly”",
-    "Q: Who created GNU/Linux?"
-];
-var triviaAnswers = [
-    "A: Tim Berners Lee",
-    "A:22",
-    "A: World Wide Web",
-    "A: Paris",
-    "A: Linus Torvalds"
-];
+var defaultQuestions = ["Q: Who invented the World Wide Web?","Q: In bingo, what number is represented by “two little ducks”?","Q: In a search bar what does the www mean?","Q: In which European city would you find the airport “Orly”","Q: Who created GNU/Linux?"]
+var defaultAnswers = ["A: Tim Berners Lee","A:22","A: World Wide Web","A: Paris","A: Linus Torvalds"];
+var triviaQuestions = JSON.parse(localStorage.getItem('questions'));
+var triviaAnswers = JSON.parse(localStorage.getItem('answers'));
+if (!triviaQuestions) {
+    localStorage.setItem('questions', JSON.stringify(defaultQuestions))
+}
+if (!triviaAnswers) {
+    localStorage.setItem('answers', JSON.stringify(defaultAnswers))
+}
+
 function getTrivia() {
     questionOutput = document.getElementById('questionOutput');
     currentTrivia = Math.floor(Math.random() * triviaQuestions.length);
